b1f2c0a93595ac5c80da95af300476f140658c57
yoggy <yoggy0@gmail.com>
2017-12-25 20:47:15 +0900

    Initial commit

---
+++ Assets/MainScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource audio_source;
+    float[] spectrum = new float[1024];
+
+    [SerializeField]
+    RectTransform rect_transform;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        audio_source.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+
+        float p = 0.0f;
+        float delta = AudioSettings.outputSampleRate / spectrum.Length;
+
+        for (int i = 0; i < spectrum.Length - 1; ++i)
+        {
+            var freq = delta * i;
+            if (freq <= 60000)
+            {
+                p += spectrum[i]; // 30k～60Hz
+            }
+
+            rect_transform.transform.localScale = new Vector3(p * 1, p * 1, p * 1);
+        }
+
+
+
+        Debug.Log(p);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+sketch01
+====
+![img.gif](img.gif)
+
+Copyright and license
+----
+Copyright (c) 2017 yoggy
+
+Released under the [MIT license](LICENSE.txt)
